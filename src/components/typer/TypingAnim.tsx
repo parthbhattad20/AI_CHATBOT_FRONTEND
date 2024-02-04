77dcb9f6751edcde54fc2ee0c2c5e4f61892ad6a
Parth Bhattad <bhattadparth20@gmail.com>
2024-02-04 11:20:39 +0530

    done

---
+++ src/components/typer/TypingAnim.tsx
@@ -0,0 +1,27 @@
+import { TypeAnimation } from "react-type-animation";
+
+const TypingAnim = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed once, initially
+        "Hi there from Parth Bhattad",
+        1000,
+        "I hope you are doing well",
+        2000,
+        "Thank you for visiting this project",
+        1500,
+      ]}
+      speed={50}
+      style={{
+        fontSize: "60px",
+        color: "white",
+        display: "inline-block",
+        textShadow: "1px 1px 20px #000",
+      }}
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TypingAnim;
